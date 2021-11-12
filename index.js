2228d7ad77e011ab88abda329e39e74c42f91750
SKFaria <fariafaria737@gmail.com>
2021-11-12 15:07:55 +0600

    sick

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
-const port = process.env.PORT || 5000;
-
 require('dotenv').config();
+
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const { MongoClient } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nzkru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected successfully');
+        const database = client.db('kids_shop');
+        const productsCollection = database.collection('products');
+
+
+        // get all products
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            console.log(products);
+            res.send(products);
+        });
+
+
+        // app.get('/products', async (req, res) => {
+        //     const email = req.query.email;
+        //     //const date = new Date(req.query.date).toLocaleDateString();
+
+        //     const query = { email: email }
+
+        //     const cursor = productsCollection.find(query);
+        //     const products = await cursor.toArray();
+        //     res.json(products);
+        // });
+
+        // app.post('/products', async (req, res) => {
+        //     const product = req.body;
+        //     const result = await productsCollection.insertOne(product);
+        //     console.log(result);
+        //     res.json(result)
+        // });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Baby shop!')
 })
 
 app.listen(port, () => {
-    console.log(`final server for assign, port: ${port}`)
+    console.log(`listening at baby: ${port}`)
 })
