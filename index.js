2037bc60060c657bffb528d29ab8ce0c94c2e135
SKFaria <fariafaria737@gmail.com>
2021-11-12 16:58:37 +0600

    fever

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const bodyParser = require('express');
+app.use(bodyParser.urlencoded({ extended: true }));
 const { MongoClient } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nzkru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -23,10 +26,18 @@ async function run() {
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
-            console.log(products);
+            //console.log(products);
             res.send(products);
         });
 
+        // get single products by Buy botton
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+            //console.log(product);
+        });
 
         // app.get('/products', async (req, res) => {
         //     const email = req.query.email;
