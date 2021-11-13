8445345abd0747cde55f1b2267e344f15160e513
SKFaria <fariafaria737@gmail.com>
2021-11-13 12:30:05 +0600

    current problem

--- index.js
+++ index.js
@@ -81,6 +81,17 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         });
+        //
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
 
 
         // app.get('/products', async (req, res) => {
